21f8be7c9f0ecd562d2c5dfc22cec76a30bda4bf
EdwMacias <edwardavid26@hotmail.com>
2024-03-18 09:35:21 -0500

    master created

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="navbar">
+    <ul class="flex flex-wrap">
+      <li class="mr-2">Inicio</li>
+      <li class="mr-2">Menu</li>
+      <li class="mr-2">Iniciar sesión</li>
+      <li class="mr-2">Salir</li>
+
+    </ul>
+  </div>
+</template>
