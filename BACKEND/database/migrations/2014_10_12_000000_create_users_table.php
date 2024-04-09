8a8132bc946ba561a09015d9e68a5f9e230022c3
Thomas Mauricio Rodriguez Sogamoso <thomasmauricio521@gmail.com>
2024-04-09 18:48:28 -0500

    nuevas migraciones en la base de datos campos de items, categorias, item_observations, user_roles

--- BACKEND/database/migrations/2014_10_12_000000_create_users_table.php
+++ BACKEND/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,10 +21,16 @@ return new class extends Migration {
             $table->timestamps();
         });
 
-        Schema::create('status',function (Blueprint $table){
-           $table->increments('statu_id');
-           $table->string('name',50);
-           $table->timestamps(); 
+        Schema::create('status', function (Blueprint $table) {
+            $table->increments('statu_id');
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('category_id');
+            $table->string('name', 50);
+            $table->timestamps();
         });
 
         Schema::create('users', function (Blueprint $table) {
@@ -34,21 +40,51 @@ return new class extends Migration {
             $table->string('last_name', 100);
             $table->string('email', 100)->unique();
             $table->string('password');
-            $table->integer('role_id')->unsigned();
+
             $table->integer('document_type_id')->unsigned();
             $table->boolean('email_verified_at')->default(false);
             $table->integer('statu_id')->unsigned()->default(1);
 
             $table->timestamps();
 
-
-            $table->foreign('role_id')->references("role_id")->on("roles");
-            $table->foreign('document_type_id')->references("document_type_id")->on("types_documents");
-            $table->foreign('statu_id')->references("statu_id")->on("status");
+            $table->foreign('document_type_id')->references("document_type_id")->on("types_documents")->onDelete("restrict");
+            $table->foreign('statu_id')->references("statu_id")->on("status")->onDelete("restrict");
 
         });
 
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('item_id');
+            $table->string('name', 50);
+            $table->string('serial_number', 100)->unique();
+            $table->text('description');
+            $table->string('imagen', 200);
+            $table->integer('category_id')->unsigned();
+            $table->integer('statu_id')->unsigned()->default(1);
+            $table->timestamps();
+            $table->foreign('statu_id')->references("statu_id")->on("status")->onDelete("restrict");
+            $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('restrict');
+        });
+        
+        Schema::create('item_observations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('observation');
+            $table->integer('item_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
 
+            $table->foreign('item_id')->references('item_id')->on('items')->onDelete('restrict');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('restrict');
+        });
+
+
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->increments('user_role_id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('cascade');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
     }
 
     public function down(): void
@@ -56,6 +92,10 @@ return new class extends Migration {
         Schema::dropIfExists('users');
         Schema::dropIfExists('types_documents');
         Schema::dropIfExists('roles');
+        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('item_observations');
+        Schema::dropIfExists('items');
         Schema::dropIfExists('status');
+        Schema::dropIfExists('categories');
     }
 };
