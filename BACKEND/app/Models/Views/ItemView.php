865992474f0e33657f60c3b5637e3cd5b89f5b89
Thomas Mauricio Rodriguez Sogamoso <thomasmauricio521@gmail.com>
2024-06-06 21:51:25 -0500

    fix(bug): arreglo modelo Itemview
    - Cambio de apuntamiento de tabla itemsview a audiovisual_items
    
    @unatasitaconte
    06/06/2024

--- BACKEND/app/Models/Views/ItemView.php
+++ BACKEND/app/Models/Views/ItemView.php
@@ -10,7 +10,7 @@ class ItemView extends Model
 {
     use HasFactory, HasUuids;
 
-    protected $table = "item_views";
+    protected $table = "audiovisual_items";
 
     protected $primaryKey = "item_id";
 
