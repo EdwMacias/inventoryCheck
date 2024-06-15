263f7af852cfc97abb2dd34f698f9c8334070ec3
Thomas Mauricio Rodriguez Sogamoso <thomasmauricio521@gmail.com>
2024-06-15 14:45:16 -0500

    build(package): eliminación nuxt/image
    - Se borro dependencia de nuxt/image
    
    @unatasitaconte
    15/06/2024

--- FRONTEND/nuxt.config.ts
+++ FRONTEND/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         ErrorMessage: 'VeeErrorMessage',
       },
     },
-  ], '@nuxt/image'],
+  ]],
   ssr: true,
   pinia: {
     storesDirs: ["./stores/**"]
