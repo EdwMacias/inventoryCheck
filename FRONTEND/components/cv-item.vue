e2cd7cec0fe4c15fc01dc15aebd68927bc34becc
Edubuntu <edwardavid26@hotmail.com>
2024-12-04 12:21:27 -0500

    esquema de uso para hdv pendiente por terminar pero consumiendo datos

--- FRONTEND/components/cv-item.vue
+++ FRONTEND/components/cv-item.vue
@@ -1,16 +1,25 @@
 <template>
-<dialog id="my_modal_2" class="modal">
-  <div class="modal-box">
-    <label>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </label>
+<dialog :open="isCvOpen" id="my_modal_2" class="modal">
+  <div class="modal-box w-11/12 max-w-5xl">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"  @click="closeCv">✕</button>
+      <h3 class="font-bold text-lg">Hoja de vida</h3>
+      <p>{{ props }}</p>
+      <div class="grid grid-rows-2">
+        <div class="">
+          <img :src="props.image" alt="Imagen del item" class="w-1/2 h-1/2 object-contain" />
+        </div>
+        <div class="">
+          <p>{{ props.itemName }}</p>
+          <p>{{ props.quantity }} {{ props.unit }}</p>
+          <p>{{ props.serial }}</p>
+          <p>{{ props.identifier }}</p>
+          <p>{{ props.category == '1' ? 'Equipo' : 'Oficina' }}</p>
+        </div>
+      </div>
+
   </div>
   <form method="dialog" class="modal-backdrop">
-    <button>close</button>
+    <button @click="closeCv">Cerrar</button>
   </form>
 </dialog>
 </template>
@@ -20,6 +29,13 @@ const emit = defineEmits(['close']);
 const props = defineProps<{
   isCvOpen: boolean;
   idCv: string;
+  itemName: string;
+  image: string;
+  category: string;
+  identifier: number;
+  serial: string;
+  quantity: number;
+  unit: string;
 }>();
 
 
